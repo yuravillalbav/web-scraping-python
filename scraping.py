f1b58b3c279e0c308dea07735d31dd9c2f05e6d2
Yura Villalba Villa <yuranisvillalba@gmail.com>
2023-08-09 21:41:32 -0500

    Code update.

--- scraping.py
+++ scraping.py
@@ -1,5 +1,5 @@
 import requests
-import csv
+import pandas as pds
 from bs4 import BeautifulSoup
 
 # GET request to extract the data.
@@ -7,21 +7,28 @@ web = 'https://trii.co/stock-list#companies'
 result = requests.get(web)
 content = result.text
 
-#Extract html
-soup = BeautifulSoup(content, 'lxml')
-company = soup.find_all('div', class_ = 'card_stock')
+# Extract html
+soup = BeautifulSoup(content, 'html.parser')
+company = soup.find_all('div', class_='card_stock')
 
-#Empty list to add.
-companies = []
+# Empty list to add.
+companies = list()
+values = list()
+images = list()
 
-#Item iteration.
+# Item iteration.
 for item_company in company:
     title = item_company.find('h2').getText()
     value = item_company.find('div', class_='title').getText()
+    img = item_company.find('img').get('src')
     companies.append(title)
-    companies.append(value)
+    values.append(value)
+    images.append(img)
 
-#Create CSV file.
-with open('company.csv', 'w') as file:
-    csv_reader = csv.writer(file, delimiter=',')
-    csv_reader.writerows(zip(companies))
+
+dictionary = {'Company': companies, 'Values': values, 'Imagen': images}
+print(dictionary)
+
+# Create and save CSV file.
+columns = pds.DataFrame(dictionary, columns=['Company', 'Values', 'Imagen'])
+columns.to_csv('company_two.csv')
